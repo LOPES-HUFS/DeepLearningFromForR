e2a8761747ab9a8e678b13f86be9ff756d565e13
choosunsick <random@example.com>
2020-01-27 19:05:37 +0900

    더 빠른 걸로 교체

--- chapter_4_5.R
+++ chapter_4_5.R
@@ -66,13 +66,13 @@ numerical_gradient_W1 <- function(f,x,t){
   h <- 1e-4
   vec <- matrix(0, nrow = nrow(W1) ,ncol = ncol(W1))
   for(i in 1:length(W1)){
-    origin <- W1
+    origin <- W1[i]
     W1[i] <<- (W1[i] + h)
     fxh1 <- f(x, t)
     W1[i] <<- (W1[i] - (2*h))
     fxh2 <- f(x, t)
     vec[i] <- (fxh1 - fxh2) / (2*h)
-    W1 <<- origin
+    W1[i] <<- origin
   }
   return(vec)
 }
@@ -80,13 +80,13 @@ numerical_gradient_W2 <- function(f,x,t){
   h <- 1e-4
   vec <- matrix(0, nrow = nrow(W2) ,ncol = ncol(W2))
   for(i in 1:length(W2)){
-    origin <- W2
+    origin <- W2[i]
     W2[i] <<- (W2[i] + h)
     fxh1 <- f(x, t)
     W2[i] <<- (W2[i] - (2*h))
     fxh2 <- f(x, t)
     vec[i] <- (fxh1 - fxh2) / (2*h)
-    W2 <<- origin
+    W2[i] <<- origin
   }
   return(vec)
 }
@@ -94,13 +94,13 @@ numerical_gradient_b1 <- function(f,x,t){
   h <- 1e-4
   vec <- matrix(0, nrow = nrow(b1) ,ncol = ncol(b1))
   for(i in 1:length(b1)){
-    origin <- b1
+    origin <- b1[i]
     b1[i] <<- (b1[i] + h)
     fxh1 <- f(x, t)
     b1[i] <<- (b1[i] - (2*h))
     fxh2 <- f(x, t)
     vec[i] <- (fxh1 - fxh2) / (2*h)
-    b1 <<- origin
+    b1[i] <<- origin
   }
   return(vec)
 }
@@ -109,13 +109,13 @@ numerical_gradient_b2 <- function(f,x,t){
   h <- 1e-4
   vec <- matrix(0, nrow = nrow(b2) ,ncol = ncol(b2))
   for(i in 1:length(b2)){
-    origin <- b2
+    origin <- b2[i]
     b2[i] <<- (b2[i] + h)
     fxh1 <- f(x, t)
     b2[i] <<- (b2[i] - (2*h))
     fxh2 <- f(x, t)
     vec[i] <- (fxh1 - fxh2) / (2*h)
-    b2 <<- origin
+    b2[i] <<- origin
   }
   return(vec)
 }
