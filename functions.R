b16e75369e5161025cf393678d2d25dea6e7802c
choosunsick <random@example.com>
2020-02-07 19:22:50 +0900

    함수 추가

--- functions.R
+++ functions.R
@@ -13,3 +13,42 @@ numerical_gradient <- function(f,x){
     }
     return(matrix(vec, nrow = nrow(x) ,ncol = ncol(x)))
 }
+
+
+cross_entropy_error <- function(y, t){
+    delta <- 1e-7
+    batchsize <- dim(y)[1]
+    return(-sum(t * log(y + delta))/batchsize)
+}
+
+cross_entropy_error_single <- function(y, t) {
+    delta <- 1e-7
+    return(-sum(t*log(y+delta)))
+}
+
+sigmoid <- function(x){
+    return(1 / (1 + exp(-x)))
+}
+
+softmax_single <- function(a){
+    c <- max(a)
+    sum_exp_a <- sum(exp(a - c))
+    return(exp(a - c) / sum_exp_a) 
+}
+
+
+softmax <- function(a){
+    exp_a <- exp(a - apply(a,1,max))
+    return(sweep(exp_a,1,rowSums(exp_a),"/"))
+}
+
+loss <-function(x,t){
+    return(cross_entropy_error(predict(x),t))
+}
+
+model.evaluate <- function(x,t){
+    y <- max.col(predict(x))
+    t <- max.col(t)
+    accuracy <- (sum(ifelse(y==t,1,0))) / dim(x)[1]
+    return(accuracy)
+}
