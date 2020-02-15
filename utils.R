1490432546c2ee841126aff417c24abb18c9c299
choosunsick <random@example.com>
2020-02-15 19:09:12 +0900

    주석 추가

--- utils.R
+++ utils.R
@@ -1,4 +1,5 @@
 making_one_hot_label <-function(t_label,nrow,ncol){
+  #use example: making_one_hot_label(t_train,60000,10)
   data <- matrix(FALSE,nrow = nrow,ncol = ncol)
   t_index <- t_label+1
   for(i in 1:NROW(data)){
@@ -8,6 +9,7 @@ making_one_hot_label <-function(t_label,nrow,ncol){
 }
 
 draw_image <- function(x){
+  #use example: draw_image(x_test[5,])
   return(image(1:28, 1:28, matrix(x, nrow=28)[ , 28:1], col = gray(seq(0, 1, 0.05)), xlab = "", ylab=""))
 }
 
