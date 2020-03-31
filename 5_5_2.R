5fb05f3a3df055aaffd3be8a2b78576d3e4ea4e6
choosunsick <random@example.com>
2020-03-31 16:57:12 +0900

    불필요한 스크립트 삭제

--- 5_5_2.R
+++
@@ -1,9 +0,0 @@
-sigmoid.forward <- function(x){
-  out <- 1 / (1+exp(-x))
-  return(list(out=out))
-}
-
-sigmoid.backward <- function(forward,dout){
-  dx <- dout*(1 - forward$out)*forward$out
-  return(list(dx=dx))
-}
