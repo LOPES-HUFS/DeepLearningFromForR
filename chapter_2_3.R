676674a820fb675c7df5c6a5934af1224524a4f6
Jaehwan <dialektike@gmail.com>
2020-04-04 17:07:16 +0900

    공백 추가

--- chapter_2_3.R
+++ chapter_2_3.R
@@ -1,8 +1,9 @@
-#3층신경망 구현 스크립트입니다. 
-#코드 설명과 사용은 다음 링크(https://choosunsick.github.io/post/softmax_function/)를 참조하세요.
+# 3층신경망 구현 스크립트입니다. 
+# 코드 설명과 사용은 다음 링크(https://choosunsick.github.io/post/softmax_function/)를 참조하세요.
+#
+# 3층 신경망 모델의 초깃값을 만들어 주는 함수입니다. 
+# 이 초기값은 3층인 신경망 모델에서만 사용됩니다.
 
-#3층 신경망 모델의 초깃값을 만들어 주는 함수입니다. 
-#이 초기값은 3층인 신경망 모델에서만 사용됩니다. 
 init <- function(){
   W1 <- matrix(seq(0.1,0.6,0.1), nrow = 2, ncol = 3)
   b1 <- matrix(seq(0.1,0.3,0.1), nrow = 1, ncol = 3)
@@ -15,8 +16,9 @@ init <- function(){
   return(model)
 }
 
-#3층 신경망 모델 계산 과정입니다.
-#초기값을 생성한 모델과 입력 데이터 값을 인자로 받아 사용합니다.  
+# 3층 신경망 모델 계산 과정입니다.
+# 초기값을 생성한 모델과 입력 데이터 값을 인자로 받아 사용합니다.
+
 model.forward <- function(model, x){
   a1 <- sweep(x %*% model$W1,2,model$b1,"+")
   z1 <- sigmoid(a1)
