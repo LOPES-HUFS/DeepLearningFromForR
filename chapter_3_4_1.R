dc650feaac60ceac88798f43276cc486ebcc4616
choosunsick <random@example.com>
2020-04-04 17:33:34 +0900

    공백추가

--- chapter_3_4_1.R
+++ chapter_3_4_1.R
@@ -1,14 +1,26 @@
+# 간단한 신경망 모델을 만드는 스크립트입니다. 
+# 이 스크립트의 코드에 대한 설명은 다음 링크에서 (https://choosunsick.github.io/post/neural_network_5/) 확인할 수 있습니다.
+# 이 스크립트는 함수만 있는 것이 아니라 함수의 작동 결과 코드도 존재합니다. 
+# 새롭게 정의하는 함수가 아닌 경우 `source()` 함수로 불러와 줍니다.
+
 source("./DeepLearningFromForR/functions.R")
+
+# 간단한 신경망 모델의 초기값을 생성하는 함수입니다. 
 
 simplemodel  <- function(){
   W <<- matrix(c(0.47355232, 0.85557411, 0.9977393, 0.03563661,0.84668094,0.69422093), nrow = 2)
 }
+
 simplemodel()
 W
+
+# 간단한 신경망 모델의 가중치를 계산하는 과정입니다.
 
 simplemodel.forward <- function(x) {
   return(x %*% W)
 }
+
+# 간단한 신경망 모델에 맞추어 수치미분을 계산하는 함수입니다.  
 
 numerical_gradient_simplemodel <- function(f){
   h <- 1e-4
@@ -36,3 +48,6 @@ t
 
 loss <- function(W){cross_entropy_error(softmax(simplemodel.forward (x)),t)}
 loss()
+
+dw <- numerical_gradient_simplemodel(loss,x,t)
+dw
--- chapter_3_4_2.R
+++ chapter_3_4_2.R
@@ -9,6 +9,7 @@ source("./DeepLearningFromForR/utils.R")
 
 # 2층 신경망에 초기값을 생성하는 함수입니다.
 # 3층 신경망이 아니기에 가중치와 편향을 2개씩만 만들고 가중치에 고정값이 아닌 랜덤값을 부여하여 만듭니다. 
+
 TwoLayerNet  <- function(input_size, hidden_size, output_size, weight_init_std = 0.01) {
   W1 <<- weight_init_std*matrix(rnorm(n = input_size*hidden_size), nrow = input_size, ncol = hidden_size)
   b1 <<- matrix(rep(0,hidden_size),nrow=1,ncol=hidden_size)
@@ -19,13 +20,15 @@ TwoLayerNet  <- function(input_size, hidden_size, output_size, weight_init_std =
 
 # 2층신경망의 계산 방법입니다. 
 # 입력데이터를 인자로 받아 사용합니다. 
+
 model.forward <- function(x){
   z1 <- sigmoid(sweep((x %*% W1),2, b1,'+'))
   return(softmax(sweep((z1 %*% W2),2, b2,'+')))
 }
 
 # 손실 함수입니다. 
-# 교차엔트로피 오차함수가 모델의 예측값과 정답을 통해 손실값을 계산합니다. 
+# 교차엔트로피 오차함수가 모델의 예측값과 정답을 통해 손실값을 계산합니다.
+
 loss <-function(x,t){
   return(cross_entropy_error(model.forward(x),t))
 }
