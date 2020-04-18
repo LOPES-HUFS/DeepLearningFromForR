f247aeabfcbbcfa2c15d142d8a55d23eeae9022f
choosunsick <random@example.com>
2020-04-18 16:19:47 +0900

    함수 분리

--- functions.R
+++ functions.R
@@ -28,22 +28,6 @@ softmax <- function(a){
     return(sweep(exp_a,1,rowSums(exp_a),"/"))
 }
 
-model.forward <- function(x){
-    z1 <- sigmoid(sweep((x %*% W1),2, b1,'+'))
-    return(softmax(sweep((z1 %*% W2),2, b2,'+')))
-}
-
-loss <-function(x,t){
-    return(cross_entropy_error(model.forward(x),t))
-}
-
-model.evaluate <- function(x,t){
-    y <- max.col(model.forward(x))
-    t <- max.col(t)
-    accuracy <- (sum(ifelse(y==t,1,0))) / dim(x)[1]
-    return(accuracy)
-}
-
 softmax <- compiler::cmpfun(softmax)
 
 Relu.forward <- function(x){
@@ -79,29 +63,4 @@ SoftmaxWithLoss.forward <- function(x, t){
 SoftmaxWithLoss.backward <- function(forward, dout=1){
     dx <- (forward$y - forward$t) / dim(forward$t)[1]
     return(list(dx = dx))
-}
-
-model.backward <- function(x){
-    Affine_1_layer <- Affine.forward(params$W1, params$b1, x)
-    Relu_1_layer <- Relu.forward(Affine_1_layer$out)
-    Affine_2_layer <- Affine.forward(params$W2, params$b2, Relu_1_layer$out)
-    return(list(x = Affine_2_layer$out, Affine_1.forward = Affine_1_layer, Affine_2.forward = Affine_2_layer, Relu_1.forward = Relu_1_layer))
-}
-
-backward_loss <- function(x, t){
-    temp  <- model.backward(x) 
-    y <- temp$x
-    last_layer.forward <- SoftmaxWithLoss.forward(y, t)
-    return(list(loss = last_layer.forward$loss, softmax = last_layer.forward, predict =  temp))
-}
-
-predict.backward <- function(x){
-    return(softmax(model.backward(x)$x))
-}
-
-model.evaluate.backward <- function(x,t){
-    y <- max.col(model.backward(x)$x)
-    t <- max.col(t)
-    accuracy <- (sum(ifelse(y == t,1,0))) / dim(x)[1]
-    return(accuracy)
 }
