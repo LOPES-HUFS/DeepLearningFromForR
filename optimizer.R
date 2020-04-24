ae94586276e38b6e57c556182c990ed6365750cb
choosunsick <random@example.com>
2020-04-24 15:53:29 +0900

    옵티마이저 생성

---
+++ optimizer.R
@@ -0,0 +1,10 @@
+sgd.update <- function(params,grads,lr=0.01){
+  sgd <- function(params,grads,weight,lr=0.01){
+    params[[weight]] <- params[[weight]] - (grads[[weight]]*lr)
+    return(params[[weight]])
+  }
+  params_temp <- lapply(names(params),FUN = function(x){sgd(params = params,grads = grad,weight = x)})
+  names(params_temp) <- names(params)
+  params <- params_temp
+  return(params) 
+}
