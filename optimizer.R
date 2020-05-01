42990ba5e64245598182b0fd98a31c1f4b0ecbb7
choosunsick <random@example.com>
2020-05-01 16:46:05 +0900

    adagrad 추가

--- optimizer.R
+++ optimizer.R
@@ -20,3 +20,20 @@ momentum.update <- function(params, grad,v, lr = 0.01,momentum=0.9){
   names(optimizer$Momentum) <- names(params)
   return(params)
 }
+
+AdaGrad.update <- function(params,grad,h,lr=0.01){
+  if (is.null(h) == TRUE) {
+    h <- rep(list(NA),NROW(params))
+    names(h) <- names(params)
+    for(i in 1:NROW(params)){
+      h[[i]] <- matrix(0,dim(params[[i]])[1],dim(params[[i]])[2])
+    }
+  }
+  
+  for(i in 1:NROW(params)){
+    optimizer$AdaGrad[[i]]  <<- h[[i]] + grad[[i]]^2
+    params[[i]] <- params[[i]] - (lr*grad[[i]] / (sqrt(optimizer$AdaGrad[[i]])+1e-7))
+  }
+  names(optimizer$AdaGrad) <- names(params)
+  return(params)
+}
