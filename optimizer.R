2a20b7c9a89c4368dcb6826ceeb6640bf9646ec2
choosunsick <random@example.com>
2020-05-01 16:43:10 +0900

    모멘텀 추가

--- optimizer.R
+++ optimizer.R
@@ -1,11 +1,22 @@
-sgd_cal <- function(params, grads, weight, lr = 0.01){
-  params[[weight]] <- params[[weight]] - (grads[[weight]]*lr)
-  return(params[[weight]])
+sgd.update <- function(params, grads, lr = 0.01){
+  for(i in names(params)){params[[i]] <- params[[i]] - (grads[[i]]*lr)}
+  return(params) 
 }
 
-sgd.update <- function(params, grads, lr = 0.01){
-  params_temp <- lapply(names(params), FUN = function(x){sgd_cal(params = params,grads = grad,weight = x,lr=lr)})
-  names(params_temp) <- names(params)
-  params <- params_temp
-  return(params) 
+optimizer <- list(c(NULL), c(NULL), c(NULL))
+names(optimizer) <- c("Momentum", "AdaGrad", "Adam")
+
+momentum.update <- function(params, grad,v, lr = 0.01,momentum=0.9){
+  if (is.null(v) == TRUE) {
+    v <- rep(list(NA),NROW(params))
+    names(v) <- names(params)
+    for(i in 1:NROW(params)){
+      v[[i]] <- matrix(0,dim(params[[i]])[1],dim(params[[i]])[2])
+    }
+  }
+  for(i in 1:NROW(params)){
+    optimizer$Momentum[[i]] <<- momentum*v[[i]] - lr*grad[[i]]
+    params[[i]] <- params[[i]]+v[[i]]}
+  names(optimizer$Momentum) <- names(params)
+  return(params)
 }
